6143abc3f25e33262dfc37b1a5ba03f877da2e41
Ed <ed@polarity.io>
2019-05-14 20:23:27 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Polarity String Decoder Integration
+
+![mode:on demand only](https://img.shields.io/badge/mode-on%20demand%20only-blue.svg)
+
+![image](images/indicator-tab.png)
+
+The Polarity string decoder integration can Currently supports decoding of Base64 and URL Encoded strings
+
+## About Polarity
+
+Polarity is a memory-augmentation platform that improves and accelerates analyst decision making.  For more information about the Polarity platform please see:
+
+https://polarity.io/
---
+++ config/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  name: 'Decoder',
+  acronym: 'DECODE',
+  description: 'Identifies encoded strings and decodes them',
+  logging: {
+    level: 'info' //trace, debug, info, warn, error, fatal
+  },
+  customTypes: [
+    {
+      key: 'base64',
+      regex: /(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)/
+    },
+    {
+      key: 'urlencoding',
+      regex: /^(?:[^%]|%[0-9A-Fa-f]{2})+$/
+    }
+  ],
+  block: {
+    component: {
+      file: './components/decoder-block.js'
+    },
+    template: {
+      file: './templates/decoder-block.hbs'
+    }
+  }
+};
